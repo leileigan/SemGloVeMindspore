9c514d0ffc6512916bebac8421d9c1e6cc48a866
ganleilei <leileigan@zju.edu.cn>
2022-11-08 11:49:40 +0000

    Add running script

--- sh/gen_mlm_coo.sh
+++ sh/gen_mlm_coo.sh
@@ -1,8 +1,9 @@
-CUDA_VISIBLE_DEVICES=2 python src/bert_cooccur.py --corpus_name xaa \
---corpus_path /home/ganleilei/data/BertGloVe/wiki/ \
---model_name roberta-large \
+CUDA_VISIBLE_DEVICES=2 python src/bert_cooccur_mindspore.py \
+--corpus_name sample \
+--corpus_path /data/home/ganleilei/BertGloVe/wiki/ \
+--model_name bert-base-uncased \
 --bert_path /home/ganleilei/data/bert/ \
---save_path /home/ganleilei/data/BertGloVe/wiki/ \
+--save_path /data/home/ganleilei/BertGloVe/wiki/ \
 --divide \
 --window_size 20 \
 --mlm_glove \
